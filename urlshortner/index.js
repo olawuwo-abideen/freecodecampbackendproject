62b589fb8521e837a8bce435f41f5ed3cc76ba60
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-02-19 15:30:27 +0100

    fix

--- urlshortner/index.js
+++ urlshortner/index.js
@@ -1,14 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyparser = require('body-parser');
+const isUrl = require('is-url');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
 
+let counter = 0;
+const shortendUrls = {};
+
+app.post('/api/shorturl', function(req, res){
+  const url = req.body.url;
+  if (!isUrl(url)){
+    res.send({error: 'invalid url'});
+    return
+  }
+  counter += 1;
+  shortendUrls[counter] = url;
+  res.send({original_url: req.body.url, short_url:
+  counter})
+});
+
+app.get('/api/shorturl/:id', function(req, res) {
+  const id = req.params.id;
+  const url = shortendUrls[id]
+  res.redirect(url);
+});
+
+ 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
